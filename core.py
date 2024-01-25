c6832add5cbb16e32dc0badb851095f312e9021d
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2024-01-25 15:25:00 -0700

    Added io lines, debugging.
    They have the zoomies and jump all over the place. NOt quite there yet...

--- core.py
+++ core.py
@@ -65,10 +65,24 @@ class ChipDesign:
 		self.reversal = {} # Specs for how build reversal of spiral at center
 		self.tlin = {} # TLIN specifications
 		self.is_etch = False # If true, regions specify etch. Otherwise, regions specify metal/etc layer presence. Equivalent to whether or not design is inverted.
+		self.io = {} # Rules for building IO components
 		
 		self.lib = gdstk.Library()
 		self.main_cell = self.lib.new_cell("MAIN")
 		self.layers = {"NbTiN": 10, "Edges": 20}
+		
+		# Updated parameters
+		self.corner_bl = (-1, -1)
+		self.corner_tr = (-1, -1)
+		self.pad_height = -1
+	
+	def update(self):
+		""" Update automatically calcualted parameters """
+		
+		self.corner_bl = (-self.chip_size_um[0]//2, -self.chip_size_um[1]//2)
+		self.corner_tr = (self.chip_size_um[0]//2, self.chip_size_um[1]//2)
+		
+		self.pad_height = self.io['pads']['chip_edge_buffer_um'] + self.io['pads']['height_um'] + self.io['pads']['taper_height_um']
 	
 	def read_conf(self, filename:str):
 		
@@ -89,11 +103,16 @@ class ChipDesign:
 			fdk = file_data[k]
 			logging.debug(f"Writing value {MPrC}{fdk}{StdC} to variable <{MPrC}{k}{StdC}> {filename}")
 		
+		self.update()
+		
 		# Return
 		return True
 	
 	def set(self, param:str, val):
-		pass
+		
+		#TODO: Implement
+		
+		self.update()
 	
 	def build(self):
 		""" Creates the chip design from the specifications. """
@@ -133,22 +152,32 @@ class ChipDesign:
 		Xc2 = center_circ_diameter/2*np.cos(theta_circ2)+center_circ_diameter/2
 		Yc2 = center_circ_diameter/2*np.sin(theta_circ2)
 		
+		spiral_y_offset = 1000
+		
 		# Change format
-		path_list1 = [(x_, y_) for x_, y_ in zip(X1, Y1)]
+		path_list1 = [(x_, y_+spiral_y_offset) for x_, y_ in zip(X1, Y1)]
 		path_list1.reverse()
-		path_list2 = [(x_, y_) for x_, y_ in zip(X2, Y2)]
-		circ_list1 = [(x_, y_) for x_, y_ in zip(Xc1, Yc1)]
+		path_list2 = [(x_, y_+spiral_y_offset) for x_, y_ in zip(X2, Y2)]
+		circ_list1 = [(x_, y_+spiral_y_offset) for x_, y_ in zip(Xc1, Yc1)]
 		circ_list1.reverse()
-		circ_list2 = [(x_, y_) for x_, y_ in zip(Xc2, Yc2)]
+		circ_list2 = [(x_, y_+spiral_y_offset) for x_, y_ in zip(Xc2, Yc2)]
 		path_list = path_list1 + circ_list1 + circ_list2 + path_list2
 		
 		# Create FlexPath object for full spiral + reversal
 		path = gdstk.FlexPath(path_list, self.tlin['Wcenter_um'], tolerance=1e-2, layer=self.layers["NbTiN"])
 		
 		# Invert selection if color is etch
-		corner_bl = (-self.chip_size_um[0]//2, -self.chip_size_um[1]//2)
-		corner_tr = (self.chip_size_um[0]//2, self.chip_size_um[1]//2)
-		bulk = gdstk.rectangle(corner_bl, corner_tr, layer=self.layers['Edges'])
+		bulk = gdstk.rectangle(self.corner_bl, self.corner_tr, layer=self.layers['Edges'])
+		
+		# ---------------------------------------------------------------------
+		# Build IO structures (meandered lines and bond pads)
+		
+		# Meander outer line: starts at (X2 and Y2)
+		self.build_io_component(path_list2[-1], self.io['outer'])
+		
+		
+		# ---------------------------------------------------------------------
+		# Add objects to chip design
 		
 		if self.is_etch:
 			logging.info(f"Inverting layers to calculate etch pattern.")
@@ -161,6 +190,173 @@ class ChipDesign:
 			logging.info(f"Adding metal layers (Non-inverted)")
 			self.main_cell.add(path)
 			self.main_cell.add(bulk)
+	
+	def calc_taper_width(self, z:float):
+		""" Calculates the width of the line given the specified taper. """
+		
+		#TODO: Implement this!
+		
+		return self.tlin['Wcenter_um']
+		
+	
+	def build_io_component(self, start_point, location_rules:dict):
+		""" Builds the meandered lines and bond pad for one conductor"""
+		
+		# Get offset parameters
+		baseline_offset = self.chip_size_um[1]//2 # Offset to translate (y = 0) to actual bottom of chip
+		just_offset = self.chip_size_um[0]//2 # Offset to translate (x = 0) to actual left side of chip
+		
+		# Constants defining states for state machine
+		SEC_VERT_PAD = 0
+		SEC_HORIZ = 1
+		SEC_VERT_SPIRAL = 2
+		
+		# Error checking
+		if self.io['curve_radius_um'] > location_rules['y_line_offset_um']:
+			logging.error("Failed to create IO component.")
+			return False
+		
+		# Height of horizontal component of line
+		line_height = self.pad_height + location_rules['y_line_offset_um']
+		
+		# Calculate trigger points for the two bends
+		y_bend_pad_trigger = line_height - self.io['curve_radius_um'] - baseline_offset
+		x_bend_spiral_trigger = start_point[0] + self.io['curve_radius_um']
+		
+		# Pre-calculate bend coordinates - Pad side
+		theta_bp = np.linspace(0, PI/2, self.io['num_points_bend'])
+		Xbp = self.io['num_points_bend']*np.cos(theta_bp)
+		Ybp = self.io['num_points_bend']*np.sin(theta_bp)
+		bend_pad = [[x_-just_offset+location_rules['x_pad_offset_um']-self.io['curve_radius_um'], y_-baseline_offset+line_height-self.io['curve_radius_um']] for x_, y_ in zip(Xbp, Ybp)] # List of points for pad side bend
+		
+		# Pre-calculate bend coordinates - Spiral side
+		theta_bp = np.linspace(PI, 3*PI/2, self.io['num_points_bend'])
+		Xbp = self.io['num_points_bend']*np.cos(theta_bp)
+		Ybp = self.io['num_points_bend']*np.sin(theta_bp)
+		bend_spiral = [[x_+start_point[0]+self.io['curve_radius_um'], y_-baseline_offset+line_height] for x_, y_ in zip(Xbp, Ybp)]
+		bend_spiral.reverse() # List of points for spiral side bend
+		
+		# Get current point on line
+		current_point = [location_rules['x_pad_offset_um']-just_offset, self.pad_height-baseline_offset]
+		
+		# Create list of points and widths
+		point_list = [current_point]
+		width_list = [self.io['pads']['taper_width_um']]
+		
+		# Record which section of line algorithm is on
+		section = SEC_VERT_PAD
+		
+		# Record how long along path you are, so know what taper width should be
+		dist = 0 
+		
+		# Run until break
+		while True:
+			
+			# 
+			if section == SEC_VERT_PAD:
+				
+				# Check when need to add first bend
+				if current_point[1]+self.io['taper']['segment_length_um'] >= y_bend_pad_trigger:
+					
+					# Loop over each point in bend, add all
+					for bp in bend_pad:
+						
+						# Calculate distance to new point
+						dX = np.sqrt((bp[0] - current_point[0])**2 + (bp[1] - current_point[1])**2)
+						
+						# Update positon
+						current_point = bp
+						dist += dX
+						
+						# Add to lists
+						point_list.append(current_point)
+						width_list.append(self.calc_taper_width(dist))
+					
+					section = SEC_HORIZ
+				else:
+					# Update position
+					current_point[1] += self.io['taper']['segment_length_um']
+					dist += self.io['taper']['segment_length_um']
+					
+					# Add to lists
+					point_list.append(current_point)
+					width_list.append(self.calc_taper_width(dist))
+			
+			elif section == SEC_HORIZ:
+				
+				# Check when need to add first bend
+				if current_point[0]+self.io['taper']['segment_length_um'] <= x_bend_spiral_trigger:
+					
+					# Loop over each point in bend, add all
+					for bp in bend_spiral:
+						
+						# Calculate distance to new point
+						dX = np.sqrt((bp[0] - current_point[0])**2 + (bp[1] - current_point[1])**2)
+						
+						# Update positon
+						current_point = bp
+						dist += dX
+						
+						# Add to lists
+						point_list.append(current_point)
+						width_list.append(self.calc_taper_width(dist))
+					
+					section = SEC_VERT_SPIRAL
+				else:
+					# Update position
+					current_point[0] -= self.io['taper']['segment_length_um']
+					dist += self.io['taper']['segment_length_um']
+					
+					# Add to lists
+					point_list.append(current_point)
+					width_list.append(self.calc_taper_width(dist))
+			
+			if section == SEC_VERT_SPIRAL:
+				
+				# Check when need to add first bend
+				if current_point[1]+self.io['taper']['segment_length_um'] >= start_point[1]:
+					
+					# Loop over each point in bend, add all
+					for bp in bend_pad:
+						
+						# Calculate distance to new point
+						dX = np.sqrt((start_point[0] - current_point[0])**2 + (start_point[1] - current_point[1])**2)
+						
+						# Update positon
+						current_point = start_point
+						dist += dX
+						
+						# Add to lists
+						point_list.append(current_point)
+						width_list.append(self.tlin['Wcenter_um'])
+					
+					break
+				else:
+					# Update position
+					current_point[1] += self.io['taper']['segment_length_um']
+					dist += self.io['taper']['segment_length_um']
+					
+					# Add to lists
+					point_list.append(current_point)
+					width_list.append(self.calc_taper_width(dist))
+		
+		# Initialize IO structure with bond pad
+		io_line = gdstk.FlexPath((location_rules['x_pad_offset_um']-just_offset, self.io['pads']['chip_edge_buffer_um']-baseline_offset), self.io['pads']['width_um'], layer=self.layers["NbTiN"])
+		io_line.segment((location_rules['x_pad_offset_um']-just_offset, self.io['pads']['chip_edge_buffer_um']+self.io['pads']['height_um']-baseline_offset), self.io['pads']['width_um'])
+		io_line.segment((location_rules['x_pad_offset_um']-just_offset, self.io['pads']['chip_edge_buffer_um']+self.io['pads']['height_um']+self.io['pads']['taper_height_um']-baseline_offset), self.io['pads']['taper_width_um'])
+		
+		# Add points and widths to curve
+		for pt,w in zip(point_list, width_list):
+			
+			io_line.segment(pt, w)
+		
+		# Add objects to chip design
+		self.main_cell.add(io_line)
+		
+		
+		
+		# (self.corner_bl[0]+self.io['outer']['x_offset_um'], self.corner_bl[1]+self.io['outer']['y_offset_um'])
+		
 	
 	def write(self, filename:str):
 		
