b22c2f193de7eb4052d0f95f76581f6e370d78e5
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2024-03-07 13:57:01 -0700

    Beginning prep for new bond pad launches

--- Chip_2024Q1.py
+++ Chip_2024Q1.py
@@ -2,7 +2,7 @@ from core import *
 
 chip = ChipDesign()
 
-chip.read_conf(os.path.join("designs", "C2024Q1_AusfB.json"))
+chip.read_conf(os.path.join("designs", "C2024Q1_AusfC.json"))
 
 # chip.add_design(os.path.join("graphics", "nist.gds"), [100, -1])
 
