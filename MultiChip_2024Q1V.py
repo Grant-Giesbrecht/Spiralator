8f9df66507043a5dbcec6c1a42830d76ff069b92
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2024-04-16 14:53:33 -0600

    Made changes to chip designs

--- MultiChip_2024Q1V.py
+++ MultiChip_2024Q1V.py
@@ -32,17 +32,29 @@ chip_d3.read_conf(os.path.join("designs", "MC-2024Q1V-D3-AusfA.json"))
 chip_d3.build()
 
 # Add first chip graphics
+# text_size = 125
+# line_gap = 35
+# baseline = -4800
+# justify_line = -1600
+
 text_size = 125
 line_gap = 35
-baseline = -4800
+baseline = 3500
 justify_line = -1600
+
+# chip_d3.insert_text((justify_line, baseline+line_gap*3+3*text_size), "C-2024-A", selected_font, text_size)
+# chip_d3.insert_text((justify_line, baseline+line_gap*2+2*text_size), "FREQUENCY CONVERTER", selected_font, text_size)
+# chip_d3.insert_text((justify_line, baseline+line_gap+text_size), f"UNLOADED | W = 3 um", selected_font, text_size)
+
+# chip_d3.insert_graphic((600, -4800), os.path.join("assets", "graphics", "CU.gds"), 350)
+# chip_d3.insert_graphic((-1600, -4800), os.path.join("assets", "graphics", "NIST.gds"), 1000, read_layer=10)
 
 chip_d3.insert_text((justify_line, baseline+line_gap*3+3*text_size), "C-2024-A", selected_font, text_size)
 chip_d3.insert_text((justify_line, baseline+line_gap*2+2*text_size), "FREQUENCY CONVERTER", selected_font, text_size)
 chip_d3.insert_text((justify_line, baseline+line_gap+text_size), f"UNLOADED | W = 3 um", selected_font, text_size)
 
 chip_d3.insert_graphic((600, -4800), os.path.join("assets", "graphics", "CU.gds"), 350)
-chip_d3.insert_graphic((-1600, -4800), os.path.join("assets", "graphics", "NIST.gds"), 1000, read_layer=10)
+chip_d3.insert_graphic((600, -4300), os.path.join("assets", "graphics", "NIST.gds"), 1000, read_layer=10)
 
 #------------------------- Create Master Design ---------------------
 
